ab803bc48059d1ef333e7698d631d65695a5731b
Martin Schmidt <martin@scoding.dk>
2021-11-25 07:56:05 +0000

    Terraform ignore tag changes on managed identity.

--- infrastructure/azure/az_aks_managed_identity.tf
+++ infrastructure/azure/az_aks_managed_identity.tf
@@ -6,4 +6,12 @@ resource "azurerm_user_assigned_identity" "aks_mid" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   name                = var.azure_aks_spn_name
+  
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      tags,
+    ]
+  }
 }
